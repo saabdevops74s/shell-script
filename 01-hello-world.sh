d835fd8facf7b720a5e9f1e2f205e373a91bad0f
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-08-29 22:39:31 +0530

    Testing

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,6 +2,4 @@
 
 echo "Hello world, I am learning DevOps with AWS"
 echo "Hello world, I am learning DevOps with AWS"
-echo "Hello world, I am learning DevOps with AWS"
-echo "Hello world, I am learning DevOps with AWS"
 
