58159b0a0afd7fcad2803e8aa001640237ac79f5
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-08-29 21:34:36 +0530

    hello changes

---
+++ 01-hello-world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello world, I am learning DevOps with AWS"
+echo "Hello world, I am learning DevOps with AWS"
+echo "Hello world, I am learning DevOps with AWS"
+echo "Hello world, I am learning DevOps with AWS"
+
