52cc9370ac1cb5187701c2677d298645d5afb52f
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-09-01 20:50:07 +0530

    Variable value from command line

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,7 +6,7 @@ PERSON1=$1 # this is the first value from command line
 PERSON2=$2 # this is the second value from command line
 
 
-echo "$PERSON1: Hi $PERSON2, Good Morning".
+echo "$PERSON1: Hi $PERSON2, Good Morning."
 echo "$PERSON2: Good Morning $PERSON1"
 echo "$PERSON1: How are you $PERSON2"
-echo "$PERSON2: I am good, how are you $PERSON1".
+echo "$PERSON2: I am good, how are you $PERSON1"
