f2b0cca5d257d10b1f6169fbdcd654b4b8215a7b
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-09-01 23:19:11 +0530

    Multiple Values through Array

--- 06-array.sh
+++ 06-array.sh
@@ -3,4 +3,4 @@
 # this is called array, instead of single value it can hold multiple values
 PERSONS=("Ramesh" "Suresh" "Sachin")
 
-echo "First Person: ${PERSONS[0]}"
+echo "First Person: ${PERSONS[2]}"
