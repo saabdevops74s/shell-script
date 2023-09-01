6242219446be821de8fb59559806cd2db9c2854e
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-09-01 18:48:56 +0530

    Hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,7 +2,7 @@
 
 echo "Hello world, I am learning DevOps with AWS"
 
-Echo "Ramesh: Hi Suresh, Good Morning"
+echo "Ramesh: Hi Suresh, Good Morning"
 echo "Suresh: Good Morning Ramesh"
 echo "Ramesh: How are you Suresh"
-echo "Suresh": "I am good, how are you Ramesh"
+echo "Suresh:" "I am good, how are you Ramesh"
