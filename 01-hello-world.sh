54e20ca325a6549b16bdfe72c2b97ddc477f5ed7
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-09-01 18:46:38 +0530

    Testing

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello world, I am learning DevOps with AWS"
+
+Echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh": "I am good, how are you Ramesh"
