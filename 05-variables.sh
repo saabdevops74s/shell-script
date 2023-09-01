d3ab53c97c51d90d44070085d18d42eb3fb8fa57
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-09-01 21:19:07 +0530

    Prompt through read command

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,13 +2,13 @@
 
 echo "Please enter your username:"
 
-read USERNAME # this is the variable where the value of the one user entered
+read -s USERNAME # this is the variable where the value of the one user entered
 
 #this is only for validation, we should not print in the terminal
-echo "username entered is: $USERNAME"
+echo "Username entered is: $USERNAME"
 
 echo "Please enter your password:"
 
-read PASSWORD
+read -s PASSWORD
 
-echo "password entered is: $PASSWORD"
+echo "Password entered is: $PASSWORD"
