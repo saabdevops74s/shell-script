b84e9235afc23bbc1648a69d7fedefd407600b7e
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-09-03 16:14:25 +0530

    added condition

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -5,9 +5,10 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-   echo "Error: Please run this script with root access"
-else
-   echo "Info: You are root user"
+    echo "Error: Please run this script with root access"
+    exit 1
+# else
+#     echo "Info: You are root user"
 fi
 
 yum install git -y
