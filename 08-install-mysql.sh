9f73c0a8d636559d8073a2124a2269209140baeb
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-09-03 16:09:24 +0530

    added condition

---
+++ 08-install-mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# our program goal is to install mysql
+USER=$(id -u)
+
+if [ $USER -ne 0 ]
+then
+   echo"Error: Please run this script with root access"
+else
+   echo "Info: You are root user"
+fi
+
+yum install git -y
